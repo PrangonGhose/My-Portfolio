cf4d080891981912840a77f961a916ef97e60533
Prangon Ghose <64170307+PrangonGhose@users.noreply.github.com>
2022-11-15 03:05:24 +0530

    Add JavaScript file

---
+++ script.js
@@ -0,0 +1,8 @@
+function mobileMenu() {
+  document.querySelector(".mobile-menu").classList.add("mobile-menu-active"); 
+}
+
+function backToNormal() {
+  document.querySelector(".mobile-menu").classList.remove("mobile-menu-active"); 
+}
+
