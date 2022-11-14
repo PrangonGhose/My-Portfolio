bfba10398bd6772159d6cc2332f0a07b6b3bd369
Prangon Ghose <64170307+PrangonGhose@users.noreply.github.com>
2022-11-15 04:16:34 +0530

    Fix linter errors

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function mobileMenu() {
-  document.querySelector(".mobile-menu").classList.add("mobile-menu-active"); 
+function showMobileMenu() {
+  document.querySelector('.mobile-menu').classList.add('mobile-menu-active');
 }
 
 function backToNormal() {
-  document.querySelector(".mobile-menu").classList.remove("mobile-menu-active"); 
+  document.querySelector('.mobile-menu').classList.remove('mobile-menu-active');
 }
 
+const mobileMenu = document.querySelector('.union');
+mobileMenu.addEventListener('click', showMobileMenu);
+const closingCross = document.querySelector('.cross-button');
+closingCross.addEventListener('click', backToNormal);
