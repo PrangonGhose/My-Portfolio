1665296bb6749a42ae50df453552056d0feb84c7
Victor <114885671+Vgonma@users.noreply.github.com>
2022-11-15 09:37:37 -0600

    Stop background scrolling and overflow

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function showMobileMenu() {
   document.querySelector('.mobile-menu').classList.add('mobile-menu-active');
+  document.querySelector('body').style.overflow = 'hidden';
 }
 
 function backToNormal() {
   document.querySelector('.mobile-menu').classList.remove('mobile-menu-active');
+  document.querySelector('body').style.overflow = 'visible';
 }
 
 const mobileMenu = document.querySelector('.union');
-mobileMenu.addEventListener('click', showMobileMenu);
 const closingCross = document.querySelector('.cross-button');
+const mobileMenuListItems = document.querySelectorAll('.mobile-menu-listItems');
+
+mobileMenu.addEventListener('click', showMobileMenu);
+
 closingCross.addEventListener('click', backToNormal);
+
+mobileMenuListItems.forEach((item) => {
+  item.addEventListener('click', backToNormal);
+});
