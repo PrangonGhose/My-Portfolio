0c2fcb6857db8adb64009c9e39a3b83ad7b15440
Prangon Ghose <64170307+PrangonGhose@users.noreply.github.com>
2022-11-16 23:29:31 +0530

    Add JavaScript file
    - Add dynamically loaded HTML
    - Add pop functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Mobile menu section */
+
 function showMobileMenu() {
   document.querySelector('.mobile-menu').classList.add('mobile-menu-active');
   document.querySelector('body').style.overflow = 'hidden';
@@ -18,4 +20,149 @@ closingCross.addEventListener('click', backToNormal);
 
 mobileMenuListItems.forEach((item) => {
   item.addEventListener('click', backToNormal);
+});
+
+/* Project pop up section */
+
+const projects = [
+  {
+    projectName: 'Tonic',
+    projectDescription: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid cupiditate vitae
+  tempore temporibus
+  quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A
+  assumenda
+  explicabo
+  perspiciatis soluta tempora facilis suscipit culpa quo, alias nesciunt omnis similique quisquam
+  fuga
+  consectetur
+  error laboriosam! Quis, quos repellendus.`,
+    projectImage: ['images/Tonic1.svg', 'images/Snapshoot Portfoliodesktop.png'],
+    projectTechnologies: ['html', 'CSS', 'JavaScript', 'github', 'ruby', 'Bootstraps'],
+    linkToLive: 'https://prangonghose.github.io/My-Portfolio/',
+    linkToSource: 'https://github.com/PrangonGhose/My-Portfolio.git',
+  },
+
+  {
+    projectName: 'Multi-Post Stories',
+    projectDescription: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid cupiditate vitae
+  tempore temporibus
+  quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A
+  assumenda
+  explicabo
+  perspiciatis soluta tempora facilis suscipit culpa quo, alias nesciunt omnis similique quisquam
+  fuga
+  consectetur
+  error laboriosam! Quis, quos repellendus.`,
+    projectImage: ['images/Tonic1.svg', 'images/Snapshoot Portfoliodesktop.png'],
+    projectTechnologies: ['html', 'CSS', 'JavaScript', 'github', 'ruby', 'Bootstraps'],
+    linkToLive: 'https://prangonghose.github.io/My-Portfolio/',
+    linkToSource: 'https://github.com/PrangonGhose/My-Portfolio.git',
+  },
+
+  {
+    projectName: 'Tonic',
+    projectDescription: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid cupiditate vitae
+  tempore temporibus
+  quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A
+  assumenda
+  explicabo
+  perspiciatis soluta tempora facilis suscipit culpa quo, alias nesciunt omnis similique quisquam
+  fuga
+  consectetur
+  error laboriosam! Quis, quos repellendus.`,
+    projectImage: ['images/Tonic1.svg', 'images/Snapshoot Portfoliodesktop.png'],
+    projectTechnologies: ['html', 'CSS', 'JavaScript', 'github', 'ruby', 'Bootstraps'],
+    linkToLive: 'https://prangonghose.github.io/My-Portfolio/',
+    linkToSource: 'https://github.com/PrangonGhose/My-Portfolio.git',
+  },
+
+  {
+    projectName: 'Multi-Post Stories',
+    projectDescription: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid cupiditate vitae
+  tempore temporibus
+  quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A
+  assumenda
+  explicabo
+  perspiciatis soluta tempora facilis suscipit culpa quo, alias nesciunt omnis similique quisquam
+  fuga
+  consectetur
+  error laboriosam! Quis, quos repellendus.`,
+    projectImage: ['images/Tonic1.svg', 'images/Snapshoot Portfoliodesktop.png'],
+    projectTechnologies: ['html', 'CSS', 'JavaScript', 'github', 'ruby', 'Bootstraps'],
+    linkToLive: 'https://prangonghose.github.io/My-Portfolio/',
+    linkToSource: 'https://github.com/PrangonGhose/My-Portfolio.git',
+  },
+];
+
+const body = document.querySelector('body');
+const seeProject = document.querySelectorAll('.see-more');
+
+function bodyNormal() {
+  body.style.overflow = 'visible';
+}
+
+seeProject.forEach((button) => {
+  button.addEventListener('click', () => {
+    const section = document.createElement('section');
+    section.innerHTML = `
+    <div class="pop-up-container">
+    <div class="pop-up-head">
+        <div>
+            <h1 class="project-body pop-up-title">${projects[button.id].projectName}</h1>
+        </div>
+        <div class="disabled-button">
+            <img src="images/Disableddesktop-cross.png" alt="disabled-button" class="disabled-button-image">
+        </div>
+    </div>
+    <div class="canopy-segment pop-up-canopy">
+        <h2 class="canopy">CANOPY</h2>
+        <ul class="canopy-items">
+            <li><img src="images/Counter.svg" alt=""></li>
+            <li class="item">Back End Dev</li>
+            <li><img src="images/Counter.svg" alt=""></li>
+            <li class="item">2022</li>
+        </ul>
+    </div>
+    <div class="pop-up-image">
+        <div class="mobile-project-image image-container">
+            <img src="${projects[button.id].projectImage[0]}" alt="project image">
+        </div>
+        <div class="desktop-project-image image-container">
+            <img src="${projects[button.id].projectImage[1]}" alt="project image">
+        </div>
+    </div>
+    <div class="pop-up-text">
+        <div class="pop-up-para">
+            <p class="project-description">
+              ${projects[button.id].projectDescription}
+            </p>
+        </div>
+        <div class="pop-up-list-buttons">
+            <div class="pop-up-list">
+                <ul class="coding-languages pop-up-languages">
+                </ul>
+            </div>
+            <div class="pop-up-buttons">
+                <button class="see-project pop-up-buttonItem1"><a href="${projects[button.id].linkToLive}" target="_blank" style="text-decoration: none">See live<img src="images/Icon - Exportlive.svg" alt="live link"></a></button>
+                <button class="see-project pop-up-buttonItem2"><a href="${projects[button.id].linkToSource}" target="_blank" style="text-decoration: none">See Source<img src="images/Framesource.png" alt="live source"></a></button>
+            </div>
+        </div>
+    </div>
+    </div>`;
+    section.classList.add('pop-up');
+    body.appendChild(section);
+    const technologyList = document.querySelector('.pop-up-languages');
+    for (let i = 0; i < projects[button.id].projectTechnologies.length; i += 1) {
+      const technologyItem = document.createElement('li');
+      technologyItem.classList.add('item-languages');
+      technologyItem.innerText = projects[button.id].projectTechnologies[i];
+      technologyList.appendChild(technologyItem);
+    }
+    body.style.overflow = 'hidden';
+    const crossButton = document.querySelector('.disabled-button-image');
+    crossButton.addEventListener('click', () => {
+      body.removeChild(section);
+      bodyNormal();
+    });
+  });
 });
