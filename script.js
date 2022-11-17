745e7da2457292f6448299433d40950c2a18ac64
Prangon Ghose <64170307+PrangonGhose@users.noreply.github.com>
2022-11-17 14:42:08 +0600

    Increase the text inside project description

--- script.js
+++ script.js
@@ -35,7 +35,11 @@ const projects = [
   perspiciatis soluta tempora facilis suscipit culpa quo, alias nesciunt omnis similique quisquam
   fuga
   consectetur
-  error laboriosam! Quis, quos repellendus.`,
+  error laboriosam! Quis, quos repellendus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid cupiditate vitae
+  tempore temporibus
+  quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A
+  assumenda
+  explicabo.`,
     projectImage: ['images/Tonic1.svg', 'images/Snapshoot Portfoliodesktop.png'],
     projectTechnologies: ['html', 'CSS', 'JavaScript', 'github', 'ruby', 'Bootstraps'],
     linkToLive: 'https://prangonghose.github.io/My-Portfolio/',
@@ -45,14 +49,18 @@ const projects = [
   {
     projectName: 'Multi-Post Stories',
     projectDescription: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid cupiditate vitae
-  tempore temporibus
-  quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A
-  assumenda
-  explicabo
-  perspiciatis soluta tempora facilis suscipit culpa quo, alias nesciunt omnis similique quisquam
-  fuga
-  consectetur
-  error laboriosam! Quis, quos repellendus.`,
+    tempore temporibus
+    quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A
+    assumenda
+    explicabo
+    perspiciatis soluta tempora facilis suscipit culpa quo, alias nesciunt omnis similique quisquam
+    fuga
+    consectetur
+    error laboriosam! Quis, quos repellendus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid cupiditate vitae
+    tempore temporibus
+    quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A
+    assumenda
+    explicabo.`,
     projectImage: ['images/Tonic1.svg', 'images/Snapshoot Portfoliodesktop.png'],
     projectTechnologies: ['html', 'CSS', 'JavaScript', 'github', 'ruby', 'Bootstraps'],
     linkToLive: 'https://prangonghose.github.io/My-Portfolio/',
@@ -62,14 +70,18 @@ const projects = [
   {
     projectName: 'Tonic',
     projectDescription: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid cupiditate vitae
-  tempore temporibus
-  quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A
-  assumenda
-  explicabo
-  perspiciatis soluta tempora facilis suscipit culpa quo, alias nesciunt omnis similique quisquam
-  fuga
-  consectetur
-  error laboriosam! Quis, quos repellendus.`,
+    tempore temporibus
+    quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A
+    assumenda
+    explicabo
+    perspiciatis soluta tempora facilis suscipit culpa quo, alias nesciunt omnis similique quisquam
+    fuga
+    consectetur
+    error laboriosam! Quis, quos repellendus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid cupiditate vitae
+    tempore temporibus
+    quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A
+    assumenda
+    explicabo.`,
     projectImage: ['images/Tonic1.svg', 'images/Snapshoot Portfoliodesktop.png'],
     projectTechnologies: ['html', 'CSS', 'JavaScript', 'github', 'ruby', 'Bootstraps'],
     linkToLive: 'https://prangonghose.github.io/My-Portfolio/',
@@ -79,14 +91,18 @@ const projects = [
   {
     projectName: 'Multi-Post Stories',
     projectDescription: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid cupiditate vitae
-  tempore temporibus
-  quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A
-  assumenda
-  explicabo
-  perspiciatis soluta tempora facilis suscipit culpa quo, alias nesciunt omnis similique quisquam
-  fuga
-  consectetur
-  error laboriosam! Quis, quos repellendus.`,
+    tempore temporibus
+    quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A
+    assumenda
+    explicabo
+    perspiciatis soluta tempora facilis suscipit culpa quo, alias nesciunt omnis similique quisquam
+    fuga
+    consectetur
+    error laboriosam! Quis, quos repellendus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid cupiditate vitae
+    tempore temporibus
+    quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A
+    assumenda
+    explicabo.`,
     projectImage: ['images/Tonic1.svg', 'images/Snapshoot Portfoliodesktop.png'],
     projectTechnologies: ['html', 'CSS', 'JavaScript', 'github', 'ruby', 'Bootstraps'],
     linkToLive: 'https://prangonghose.github.io/My-Portfolio/',
@@ -106,48 +122,48 @@ seeProject.forEach((button) => {
     const section = document.createElement('section');
     section.innerHTML = `
     <div class="pop-up-container">
-    <div class="pop-up-head">
+      <div class="pop-up-head">
         <div>
-            <h1 class="project-body pop-up-title">${projects[button.id].projectName}</h1>
+          <h1 class="project-body pop-up-title">${projects[button.id].projectName}</h1>
         </div>
         <div class="disabled-button">
-            <img src="images/Disableddesktop-cross.png" alt="disabled-button" class="disabled-button-image">
+          <img src="images/Disableddesktop-cross.png" alt="disabled-button" class="disabled-button-image">
         </div>
-    </div>
-    <div class="canopy-segment pop-up-canopy">
+      </div>
+      <div class="canopy-segment pop-up-canopy">
         <h2 class="canopy">CANOPY</h2>
         <ul class="canopy-items">
-            <li><img src="images/Counter.svg" alt=""></li>
-            <li class="item">Back End Dev</li>
-            <li><img src="images/Counter.svg" alt=""></li>
-            <li class="item">2022</li>
+          <li><img src="images/Counter.svg" alt=""></li>
+          <li class="item">Back End Dev</li>
+          <li><img src="images/Counter.svg" alt=""></li>
+          <li class="item">2022</li>
         </ul>
-    </div>
-    <div class="pop-up-image">
+      </div>
+      <div class="pop-up-image">
         <div class="mobile-project-image image-container">
-            <img src="${projects[button.id].projectImage[0]}" alt="project image">
+          <img src="${projects[button.id].projectImage[0]}" alt="project image">
         </div>
         <div class="desktop-project-image image-container">
-            <img src="${projects[button.id].projectImage[1]}" alt="project image">
+          <img src="${projects[button.id].projectImage[1]}" alt="project image">
         </div>
-    </div>
-    <div class="pop-up-text">
+      </div>
+      <div class="pop-up-text">
         <div class="pop-up-para">
-            <p class="project-description">
-              ${projects[button.id].projectDescription}
-            </p>
+          <p class="project-description">
+            ${projects[button.id].projectDescription}
+          </p>
         </div>
         <div class="pop-up-list-buttons">
-            <div class="pop-up-list">
-                <ul class="coding-languages pop-up-languages">
-                </ul>
-            </div>
-            <div class="pop-up-buttons">
-                <button class="see-project pop-up-buttonItem1"><a href="${projects[button.id].linkToLive}" target="_blank" style="text-decoration: none">See live<img src="images/Icon - Exportlive.svg" alt="live link"></a></button>
-                <button class="see-project pop-up-buttonItem2"><a href="${projects[button.id].linkToSource}" target="_blank" style="text-decoration: none">See Source<img src="images/Framesource.png" alt="live source"></a></button>
-            </div>
+          <div class="pop-up-list">
+            <ul class="coding-languages pop-up-languages">
+            </ul>
+          </div>
+          <div class="pop-up-buttons">
+            <button class="see-project pop-up-buttonItem1"><a href="${projects[button.id].linkToLive}" target="_blank" style="text-decoration: none">See live<img src="images/Icon - Exportlive.svg" alt="live link"></a></button>
+            <button class="see-project pop-up-buttonItem2"><a href="${projects[button.id].linkToSource}" target="_blank" style="text-decoration: none">See Source<img src="images/Framesource.png" alt="live source"></a></button>
+          </div>
         </div>
-    </div>
+      </div>
     </div>`;
     section.classList.add('pop-up');
     body.appendChild(section);
